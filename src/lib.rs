5a223958d60861f01dfc3c9a25661ad931fa5d14
CBEO <cbeok@protonmail.com>
2018-12-02 19:02:25 -0600

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,277 @@
+
+const SECS_PER_MINUTE: u32 = 60;
+const SECS_PER_HOUR: u32 = 3600;
+
+// these config variables are const right now, but should move into a struct
+const DEFAULT_WORK_MIN: u32 = 25;
+const SHORT_BFREAK_MIN: u32 = 5;
+const LONG_BREAK_MIN: u32 = 15;
+
+/// Formats seconds as HH:MM:SS
+///
+/// ```
+/// let formatted = String::from("1:02:33");
+/// let secs = 3753;
+///
+/// assert_eq!(pmdr::format_secs(secs), formatted);
+/// ```
+pub fn format_secs(secs: u32) -> String {
+    let hours = secs / SECS_PER_HOUR;
+    let minutes = (secs - SECS_PER_HOUR * hours) / SECS_PER_MINUTE;
+    let seconds = secs % SECS_PER_MINUTE;
+
+    format!("{}:{:02}:{:02}", hours, minutes, seconds)
+}
+
+struct WorkTimer {count: usize, timer: u32}
+struct Paused(Box<TimerState>);
+struct BreakTimer {count: usize, timer: u32}
+struct Stopped {count: usize}
+
+impl WorkTimer {
+    fn new(count: usize, timer: u32) -> Box<WorkTimer> {
+        Box::new(WorkTimer {
+            count,
+            timer
+        })
+    }
+}
+
+impl BreakTimer {
+    fn new(count: usize, timer: u32) -> Box<BreakTimer> {
+        Box::new(BreakTimer {
+            count,
+            timer
+        })
+    }
+}
+
+trait TimerState {
+    fn count(&self) -> usize;
+
+    fn timer(&self) -> u32;
+
+    fn ticking(&self) -> bool {
+        true
+    }
+
+    fn on_break(&self) -> bool {
+        false
+    }
+
+    fn label(&self) -> String;
+
+    fn toggle(self: Box<Self>) -> Box<dyn TimerState>;
+
+    fn tick(self: Box<Self>) -> (Box<dyn TimerState>, bool);
+
+    fn stop(self: Box<Self>) -> Box<dyn TimerState> {
+        let stopped = Stopped {count: self.count()};
+        Box::new(stopped)
+    }
+}
+
+impl TimerState for Stopped {
+    fn count(&self) -> usize {
+        self.count
+    }
+
+    fn timer(&self) -> u32 {
+        DEFAULT_WORK_MIN * SECS_PER_MINUTE
+    }
+
+    fn ticking(&self) -> bool {
+        false
+    }
+
+    fn label(&self) -> String {
+        format!("Stopped")
+    }
+
+    fn toggle(self: Box<Self>) -> Box<dyn TimerState> {
+        WorkTimer::new(self.count(), self.timer())
+    }
+
+    fn tick(self: Box<Self>) -> (Box<dyn TimerState>, bool) {
+        (self, false)
+    }
+
+    // if stop is called on Stopped, we reset the tally.
+    // this is kind of hacky
+    fn stop(self: Box<Self>) -> Box<dyn TimerState> {
+        Box::new(Stopped {count: 0})
+    }
+}
+
+impl TimerState for Paused {
+    fn count(&self) -> usize {
+        self.0.count()
+    }
+
+    fn timer(&self) -> u32 {
+        self.0.timer()
+    }
+
+    fn ticking(&self) -> bool {
+        false
+    }
+
+    fn label(&self) -> String {
+        let contained = self.0.label();
+        format!("Paused ({})", contained)
+    }
+
+    fn toggle(self: Box<Self>) -> Box<dyn TimerState> {
+        self.0
+    }
+
+    fn tick(self: Box<Self>) -> (Box<dyn TimerState>, bool) {
+        (self, false)
+    }
+}
+
+impl TimerState for WorkTimer {
+    fn count(&self) -> usize {
+        self.count
+    }
+
+    fn timer(&self) -> u32 {
+        self.timer
+    }
+
+    fn label(&self) -> String {
+        format!("Keep Going!")
+    }
+
+    fn toggle(self: Box<Self>) -> Box<dyn TimerState> {
+        Box::new(Paused(self))
+    }
+
+    fn tick(self: Box<Self>) -> (Box<dyn TimerState>, bool) {
+        let count = self.count;
+        let timer = self.timer - 1;
+
+        let new_state: Box<dyn TimerState> = if timer <= 0 {
+            let timer = if count % 4 == 0 {
+                LONG_BREAK_MIN * SECS_PER_MINUTE
+            } else {
+                SHORT_BFREAK_MIN * SECS_PER_MINUTE
+            };
+
+            BreakTimer::new(count+1,timer)
+        } else {
+            WorkTimer::new(count, timer)
+        };
+
+        (new_state, timer <= 0)
+    }
+}
+
+impl TimerState for BreakTimer {
+    fn count(&self) -> usize {
+        self.count
+    }
+
+    fn timer(&self) -> u32 {
+        self.timer
+    }
+
+    fn label(&self) -> String {
+        format!("On Break")
+    }
+
+    fn on_break(&self) -> bool {
+        true
+    }
+
+    fn toggle(self: Box<Self>) -> Box<dyn TimerState> {
+        Box::new(Paused(self))
+    }
+
+    fn tick(self: Box<Self>) -> (Box<dyn TimerState>, bool) {
+        let count = self.count;
+        let timer = self.timer - 1;
+
+        let new_state: Box<dyn TimerState> = if timer <= 0 {
+            WorkTimer::new(count, DEFAULT_WORK_MIN * SECS_PER_MINUTE)
+        } else {
+            BreakTimer::new(count,timer)
+        };
+
+        (new_state, timer <= 0)
+    }
+}
+
+pub struct PMDRApp {
+    timer_state: Option<Box<TimerState>>
+}
+
+impl PMDRApp {
+    pub fn new() -> PMDRApp {
+        let timer_state: Box<dyn TimerState> = WorkTimer::new(0, DEFAULT_WORK_MIN * SECS_PER_MINUTE);
+        let timer_state = Some(timer_state);
+
+        PMDRApp {
+            timer_state
+        }
+    }
+
+    /// Updates the timer and returns a boolean if the state changed
+    pub fn tick(&mut self) -> bool {
+        let (new_state, state_changed) = self.timer_state.take().unwrap().tick();
+
+        self.timer_state = Some(new_state);
+
+        state_changed
+    }
+
+    /// Gets the formatted string of the time remaining
+    pub fn countdown_string(&self) -> String {
+        let secs = self.timer_state
+            .as_ref()
+            .map_or(0, |state| state.timer());
+
+        format_secs(secs)
+    }
+
+    /// Toggle the app's timer between ticking and paused states
+    pub fn toggle_timer(&mut self) -> bool {
+        let new_state = self.timer_state.take().unwrap().toggle();
+        let ticking = &new_state.ticking();
+        self.timer_state = Some(new_state);
+        *ticking
+    }
+
+    /// Gets the current tally
+    pub fn tally(&self) -> usize {
+        self.timer_state
+            .as_ref()
+            .map_or(0, |state| state.count())
+    }
+
+    /// Stops the application, but keeps the tally
+    pub fn stop(&mut self) {
+        let new_state = self.timer_state.take().unwrap().stop();
+        self.timer_state = Some(new_state);
+    }
+
+    /// Returns true if the timer is ticking
+    pub fn ticking(&self) -> bool {
+        self.timer_state
+            .as_ref()
+            .map_or(false, |state| state.ticking())
+    }
+
+    pub fn state_label(&self) -> String {
+        self.timer_state
+            .as_ref()
+            .map_or_else(|| String::from(""), |state| state.label())
+    }
+
+    pub fn on_break(&self) -> bool {
+        self.timer_state
+            .as_ref()
+            .map_or(false, |state| state.on_break())
+    }
+}
+
---
+++ src/main.rs
@@ -0,0 +1,130 @@
+
+//! # PMDR
+//!
+//! A GTK+ Pomodoro application with "snazzy" reporting
+
+extern crate pmdr;
+extern crate gio;
+extern crate gtk;
+extern crate notify_rust;
+
+use notify_rust::{Notification, NotificationHandle};
+use gio::prelude::*;
+use gtk::prelude::*;
+
+
+use std::rc::Rc;
+use std::cell::RefCell;
+
+fn build_ui(app: &gtk::Application,
+            pomodoro: Rc<RefCell<pmdr::PMDRApp>>,
+            notification: Rc<RefCell<Option<NotificationHandle>>>)
+{
+    let win = gtk::ApplicationWindow::new(app);
+
+    win.set_title("PMDR");
+    win.set_border_width(10);
+    win.set_position(gtk::WindowPosition::Mouse);
+    win.set_default_size(240,310);
+
+    win.connect_delete_event(move |w,_| {
+        w.destroy();
+        Inhibit(false)
+    });
+
+    let vbox = gtk::Box::new(gtk::Orientation::Vertical,  8);
+
+    let state_label = gtk::Label::new(None);
+    state_label.set_text(&pomodoro.borrow().state_label());
+    vbox.add(&state_label);
+
+    let countdown_label = gtk::Label::new(None);
+
+    countdown_label.set_text(&pomodoro.borrow().countdown_string());
+    vbox.add(&countdown_label);
+
+    let tally_label = gtk::Label::new(None);
+
+    tally_label.set_text(&format!("Tally: {}", 0));
+    vbox.add(&tally_label);
+
+    let pause_button = gtk::Button::new_with_label("Pause");
+    let button_pomodoro = pomodoro.clone();
+
+    pause_button.connect_clicked(move |_btn| {
+        button_pomodoro.borrow_mut().toggle_timer();
+    });
+
+    vbox.add(&pause_button);
+
+    let stop_button = gtk::Button::new_with_label("Stop");
+    let stop_button_pomodoro = pomodoro.clone();
+
+    stop_button.connect_clicked(move |btn| {
+        stop_button_pomodoro.borrow_mut().stop();
+        btn.set_label("Reset Tally");
+    });
+
+    vbox.add(&stop_button);
+
+    win.add(&vbox);
+    win.show_all();
+
+    let tick = move || {
+        let state_changed = pomodoro.borrow_mut().tick();
+
+        // set button labels
+        if pomodoro.borrow().ticking() {
+            pause_button.set_label("Pause");
+            stop_button.set_label("Stop");
+        } else {
+            pause_button.set_label("Play");
+        }
+
+        // update labels
+        state_label.set_text(&pomodoro.borrow().state_label());
+        countdown_label.set_text(&pomodoro.borrow().countdown_string());
+        tally_label.set_text(&format!("Tally: {}", pomodoro.borrow().tally()));
+
+        if state_changed {
+            let on_break = pomodoro.borrow().on_break();
+            let handle  = if on_break {
+                // TODO handle error case
+                 Notification::new()
+                    .summary("TIME FOR A BREAK!")
+                    .timeout(0)
+                    .show()
+                    .unwrap()
+            } else {
+                Notification::new()
+                    .summary("HEY, GET BACK TO WORK!")
+                    .timeout(0)
+                    .show()
+                    .unwrap()
+            };
+
+            notification.replace(Some(handle));
+        }
+
+        gtk::Continue(true)
+    };
+
+    gtk::timeout_add_seconds(1, tick);
+
+}
+
+
+fn main() {
+
+    let app = gtk::Application::new("games.coolmedium.pmdr", gio::ApplicationFlags::empty())
+        .expect("Failed to initialize GTK Application");
+
+    app.connect_startup( move |app0| {
+        let pomodoro = Rc::new(RefCell::new(pmdr::PMDRApp::new()));
+        let notification = Rc::new(RefCell::new(None));
+        build_ui(app0, pomodoro, notification);
+    });
+
+    app.connect_activate(|_| {});
+    app.run(&vec![]);
+}
